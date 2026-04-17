92c6a3d3024210e9535332f8a7508c9a7f58eaae
ANSH GAUR <anshgaurx@gmail.com>
2026-04-17 22:47:16 +0530

    Update 3761. Minimum Absolute Distance Between Mirror Pairs.py

--- 3761. Minimum Absolute Distance Between Mirror Pairs.py
+++ 3761. Minimum Absolute Distance Between Mirror Pairs.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def minMirrorPairDistance(self, nums):
+        def reverse_num(x):
+            return int(str(x)[::-1])
 
+        latest_rev = {} 
+        min_dist = float('inf')
+
+        for j, num in enumerate(nums):
+            if num in latest_rev:
+                min_dist = min(min_dist, j - latest_rev[num])
+            rev = reverse_num(num)
+            latest_rev[rev] = j
+
+        return -1 if min_dist == float('inf') else min_dist
